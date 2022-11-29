0afcd5cf082fb2541c54a6c8ae4f9b70cffe33b2
LuisH <luisjunior9831@gmail.com>
2022-11-29 14:57:36 -0500

    Agregando moficacion y hook personalizado

--- src/Componentes/TaskList.jsx
+++ src/Componentes/TaskList.jsx
@@ -1,29 +1,30 @@
-import { GrAdd } from 'react-icons/gr'
-import {useState} from 'react'
+import { GrAdd } from "react-icons/gr";
+import { useState } from "react";
 
-function TaskList({createNewTask}) {
-    const [tarea, setTarea] = useState('')
-    return <>
-        <div className="Container1">
-            <form onSubmit={(e)=>{
-                
-                createNewTask(tarea)
-                setTarea("")
-            }
-                
-            }
-            >
-            <input 
-            type="text" 
-            className='text' 
+function TaskList({ createNewTask }) {
+  const [tarea, setTarea] = useState("");
+  return (
+    <>
+      <div className="Container1">
+        <form
+          onSubmit={(e) => {
+            createNewTask(tarea);
+            setTarea("");
+          }}
+        >
+          <input
+            type="text"
+            className="text"
             placeholder="Add your new todo"
-            onChange={(e) => setTarea(e.target.value)} />
-            <button id='add' >
-                <GrAdd />
-            </button>
-            </form>
-        </div>
+            onChange={(e) => setTarea(e.target.value)}
+          />
+          <button id="add">
+            <GrAdd />
+          </button>
+        </form>
+      </div>
     </>
+  );
 }
 
 export default TaskList;
